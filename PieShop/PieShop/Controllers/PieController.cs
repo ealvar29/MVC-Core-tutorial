b0674ee63a13243e35c52c39790b14f3e8aa7284
Ed <edalvarez2015@gmail.com>
2021-06-21 15:02:24 -0500

    Added Pie Controller

---
+++ PieShop/PieShop/Controllers/PieController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Interface;
+
+namespace PieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+        
+        // GET
+        public ViewResult List()
+        {
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
